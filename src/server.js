e64bd6d7e700f8b8e7d0be1da2a9544e0305aabf
tompo <tompodevin23@gmail.com>
2024-04-24 22:53:14 +0200

    finally ?

--- src/server.js
+++ src/server.js
@@ -6,10 +6,8 @@ const servicesRoutes = require('./routes/servicesRoutes')
 const clientsRoutes = require('./routes/clientsRoutes')
 const appointmentsRoutes = require('./routes/appointmentsRoutes')
 
-
 const app = express()
 app.use(express.json())
-
 
 // Connect to DB and initialize routes only once at startup
 connectToDb()
@@ -19,13 +17,11 @@ app.use('/api/services', servicesRoutes)
 app.use('/api/clients', clientsRoutes)
 app.use('/api/appointments', appointmentsRoutes)
 
-
-
-if (process.env.NODE_ENV === 'development') {
-  const PORT = process.env.PORT || 3000
-  app.listen(PORT, () => {
-    console.log(`Server ecoute sur le port ${PORT}`)
-  })
-}
+// if (process.env.NODE_ENV === 'development') {
+//   const PORT = process.env.PORT || 3000
+//   app.listen(PORT, () => {
+//     console.log(`Server ecoute sur le port ${PORT}`)
+//   })
+// }
 
 module.exports = app
