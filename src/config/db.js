88a175a319db9b20d53fde2112eb4650f8decd1e
tompo <tompodevin23@gmail.com>
2024-04-24 22:12:22 +0200

    Deploy in basic config ? hope it works

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose');
-let connection = null;
+const mongoose = require('mongoose')
+let connection = null
 
 async function connectToDb() {
   if (connection && mongoose.connection.readyState === 1) {
-    console.log('Using existing database connection');
-    return connection;
+    console.log('Using existing database connection')
+    return connection
   }
 
   if (connection && mongoose.connection.readyState !== 1) {
-    console.log('Database connection is not ready, reconnecting...');
-    await mongoose.disconnect();  // Assurer que la connexion est fermée proprement
-    connection = null;  // Réinitialiser la connexion
+    console.log('Database connection is not ready, reconnecting...')
+    await mongoose.disconnect() // Assurer que la connexion est fermée proprement
+    connection = null // Réinitialiser la connexion
   }
 
-
-  console.log('Creating new database connection...');
+  console.log('Creating new database connection...')
   try {
-    connection = await mongoose.connect(process.env.MONGO_URI);
-    console.log('Database connected successfully.');
-    return connection;
+    connection = await mongoose.connect(process.env.MONGO_URI)
+    console.log('Database connected successfully.')
+    return connection
   } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
-    throw error;  // Propager l'erreur pour arrêter l'application si nécessaire
+    console.error('Failed to connect to MongoDB:', error)
+    throw error // Propager l'erreur pour arrêter l'application si nécessaire
   }
 }
 
-module.exports = connectToDb;
+module.exports = connectToDb
--- src/server.js
+++ src/server.js
@@ -1,25 +1,19 @@
 require('dotenv').config()
 const express = require('express')
-// const cookieParser = require('cookie-parser')
 const connectToDb = require('./config/db')
-const getJwtMiddleware = require('./middleware/authMiddleware')
-const authRoutes = require('./routes/authRoutes')
 const materialsRoutes = require('./routes/materialsRoutes')
 const servicesRoutes = require('./routes/servicesRoutes')
 const clientsRoutes = require('./routes/clientsRoutes')
 const appointmentsRoutes = require('./routes/appointmentsRoutes')
 
-const jwtSecret = process.env.JWT_SECRET
 
 const app = express()
 app.use(express.json())
+
+
 // Connect to DB and initialize routes only once at startup
 connectToDb()
 
-// app.use(cookieParser())
-// app.use(getJwtMiddleware(jwtSecret))
-
-// app.use('/api', authRoutes)
 app.use('/api/materials', materialsRoutes)
 app.use('/api/services', servicesRoutes)
 app.use('/api/clients', clientsRoutes)
