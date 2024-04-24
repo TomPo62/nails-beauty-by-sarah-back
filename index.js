359ad051fca465e1211b7d5227a3feb1fe763794
tompo <tompodevin23@gmail.com>
2024-04-24 11:41:42 +0200

    Try fix two  Sarah Backend API

--- index.js
+++ index.js
@@ -32,53 +32,59 @@ app.use(
     secret: jwtToken,
     algorithms: ['HS256'],
     getToken: (req) => req.cookies.token,
-  }).unless({ path: ['/api/login', '/api/logout', '/'] })
+  }).unless({ path: ['/api/login'] })
 )
 
 app.post('/api/login', (req, res) => {
-  const { username, pwd } = req.body;
-  if ((username === process.env.USER_ONE && pwd === process.env.PWD_USER_ONE) ||
-      (username === process.env.USER_TWO && pwd === process.env.PWD_USER_TWO)) {
-    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1d' });
-    res.cookie('token', token, { httpOnly: true, secure: true });
-    res.status(200).send({ message: 'Logged in successfully!', token });
+  const { username, pwd } = req.body
+  if (
+    (username === process.env.USER_ONE && pwd === process.env.PWD_USER_ONE) ||
+    (username === process.env.USER_TWO && pwd === process.env.PWD_USER_TWO)
+  ) {
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    })
+    res.cookie('token', token, { httpOnly: true, secure: true })
+    res.status(200).send({ message: 'Logged in successfully!', token })
   } else {
-    res.status(401).send('Unauthorized');
+    res.status(401).send('Unauthorized')
   }
-});
+})
 
 app.get('/api/logout', (req, res) => {
   res.clearCookie('token')
   res.send({ message: 'Logged out successfully!' })
 })
 
-
 async function connectToDb() {
   if (cachedDb) {
-    console.log('Using cached database connection.');
-    return cachedDb;
+    console.log('Using cached database connection.')
+    return cachedDb
   }
-  console.log('Creating new database connection...');
-  const db = await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  cachedDb = db;
-  console.log('Database connected successfully.');
-  return db;
+  console.log('Creating new database connection...')
+  const db = await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  cachedDb = db
+  console.log('Database connected successfully.')
+  return db
 }
 
 exports.handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+  context.callbackWaitsForEmptyEventLoop = false
   try {
-    await connectToDb();
-    app.use('/api/materials', materialsRoutes);
-    app.use('/api/services', servicesRoutes);
-    app.use('/api/clients', clientsRoutes);
-    app.use('/api/appointments', appointmentsRoutes);
-    return serverless(app)(event, context);
+    await connectToDb()
+    app.use('/api/materials', materialsRoutes)
+    app.use('/api/services', servicesRoutes)
+    app.use('/api/clients', clientsRoutes)
+    app.use('/api/appointments', appointmentsRoutes)
+    return serverless(app)(event, context)
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    return { statusCode: 500, body: 'Internal Server Error' };
+    console.error('Error connecting to MongoDB:', error)
+    return { statusCode: 500, body: 'Internal Server Error' }
   }
-};
+}
 
 // app.listen(PORT, () => {
 //   console.log(`Server listening on port ${PORT}`)
