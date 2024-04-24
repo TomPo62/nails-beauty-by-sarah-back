046eeb10e9a464d7ddfdb8739eef46101e317af1
tompo <tompodevin23@gmail.com>
2024-04-24 22:19:56 +0200

    Deploy in basic config ? hope it works2

--- src/config/db.js
+++ src/config/db.js
@@ -1,26 +1,13 @@
 const mongoose = require('mongoose')
-let connection = null
 
 async function connectToDb() {
-  if (connection && mongoose.connection.readyState === 1) {
-    console.log('Using existing database connection')
-    return connection
-  }
-
-  if (connection && mongoose.connection.readyState !== 1) {
-    console.log('Database connection is not ready, reconnecting...')
-    await mongoose.disconnect() // Assurer que la connexion est fermée proprement
-    connection = null // Réinitialiser la connexion
-  }
-
-  console.log('Creating new database connection...')
+  console.log('Attempting to connect to database...')
   try {
-    connection = await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('Database connected successfully.')
-    return connection
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error)
-    throw error // Propager l'erreur pour arrêter l'application si nécessaire
+    throw error
   }
 }
 
