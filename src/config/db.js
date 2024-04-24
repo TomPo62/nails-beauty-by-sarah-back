caa893872011a473d3dae533ef73151cf5ddcb44
tompo <tompodevin23@gmail.com>
2024-04-24 16:32:13 +0200

    Fix the db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-global.cachedDb = global.cachedDb || null;  // Utilisez une variable globale pour stocker la connexion
-
-async function connectToDb(uri) {
-  if (global.cachedDb) {
-    console.log('Using cached database connection.');
-    return global.cachedDb;  // Retourne la connexion mise en cache
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connect to db successfully')
+  } catch (err) {
+    console.log('Connect to db failed')
   }
-
-  console.log('Creating new database connection...');
-  const db = await mongoose.connect(uri, {
-    bufferCommands: false,
-  });
-
-  global.cachedDb = db;
-  console.log('Database connected successfully.');
-  return db;
 }
 
-module.exports = connectToDb;
+module.exports = connectToDb
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const connectToDb = require('./config/db')
 const getJwtMiddleware = require('./middleware/authMiddleware')
@@ -12,12 +11,12 @@ const clientsRoutes = require('./routes/clientsRoutes')
 const appointmentsRoutes = require('./routes/appointmentsRoutes')
 
 const jwtSecret = process.env.JWT_SECRET
-const dbUri = process.env.MONGO_URI
 
 const app = express()
 app.use(express.json())
+// Connect to DB and initialize routes only once at startup
+connectToDb()
 app.use(helmet())
-app.use(cors({ credentials: true, origin: true }))
 app.use(cookieParser())
 app.use(getJwtMiddleware(jwtSecret))
 
@@ -27,11 +26,7 @@ app.use('/api/services', servicesRoutes)
 app.use('/api/clients', clientsRoutes)
 app.use('/api/appointments', appointmentsRoutes)
 
-// Connect to DB and initialize routes only once at startup
-connectToDb(dbUri).catch((err) => {
-  console.err('Failed to connect to MongoDB:', err)
-  process.exit(1)
-})
+
 
 if (process.env.NODE_ENV === 'development') {
   const PORT = process.env.PORT || 3000
