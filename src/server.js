511c0e5ff8db896b668b4ae66b15664c1908bd93
tompo <tompodevin23@gmail.com>
2024-04-27 14:41:07 +0200

    Fix the allowedOrigins for CORS

--- src/server.js
+++ src/server.js
@@ -17,9 +17,18 @@ const app = express()
 app.use(helmet())
 
 app.use(cors({
-  origin: process.env.DOMAIN,
-  credentials: true
+  origin: function(origin, callback) {
+    const allowedOrigins = ['http://localhost:5173', 'https://nails-beauty-by-sarah-back.vercel.app'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
 
 
 app.use(bodyParser.json())
