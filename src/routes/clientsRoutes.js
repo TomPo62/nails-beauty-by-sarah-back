74c08a9f525aee0589ebd09f14006e8a40ab6835
tompo <tompodevin23@gmail.com>
2024-04-29 10:15:17 +0200

    Fix Routes order to fetch recent-clients

--- src/routes/clientsRoutes.js
+++ src/routes/clientsRoutes.js
@@ -1,19 +1,13 @@
 const express = require('express')
-const {validateClient} = require('../validation/clientsValidate')
+const { validateClient } = require('../validation/clientsValidate')
 const clientsController = require('../controllers/clientsController')
 const router = express.Router()
 
-router.post(
-  '/',
-  validateClient('create'),
-  clientsController.createClient
-)
-router.put('/:id', validateClient('update'), clientsController.updateClient);
-
+router.post('/', validateClient('create'), clientsController.createClient)
+router.put('/:id', validateClient('update'), clientsController.updateClient)
+router.get('/recent-clients', clientsController.getRecentClients)
 
 router.get('/', clientsController.getAllClients)
 router.get('/:id', clientsController.getClientById)
-router.get('/recent-clients', clientsController.getRecentClients)
-
 
 module.exports = router
