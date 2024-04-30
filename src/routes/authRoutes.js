d0bbbc464dc62b42e20eb13419f00e1f609ee5be
tompo <tompodevin23@gmail.com>
2024-05-01 01:25:26 +0200

    Add status API

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,6 +7,12 @@ router.get('/logout', authController.logout);
 
 router.get('/status', (req, res) => {
   res.send({ loggedIn: true });
+}, (error, req, res, next) => {
+  if (error.name === 'UnauthorizedError') {
+    res.status(401).send({ loggedIn: false });
+  } else {
+    next(error);
+  }
 });
 
 module.exports = router;
