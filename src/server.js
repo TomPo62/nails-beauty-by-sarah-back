68697447b7ef6985b6423bef3c79fe963d368c9f
tompo <tompodevin23@gmail.com>
2024-04-30 20:44:51 +0200

    fix the server.js

--- src/server.js
+++ src/server.js
@@ -19,8 +19,7 @@ app.use(helmet())
 app.use(
   cors({
     origin: [
-      'https://localhost:5173',
-      'https://nails-beauty-by-sarah-back.vercel.app',
+      'https://localhost:5173'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
