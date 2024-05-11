021df08394367123ff0b5dc3645e0aba619fb030
tompo <tompodevin23@gmail.com>
2024-05-11 15:41:53 +0200

    Remove the vercel link in CORS Origin

--- src/server.js
+++ src/server.js
@@ -10,12 +10,22 @@ const app = express()
 
 app.use(helmet())
 
-app.use(cors({
-  origin: ['https://localhost:5173', 'https://localhost:4173','https://nails-beauty-by-sarah-back.vercel.app'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Auth-Token', 'Origin', 'Client-Security-Token', 'Accept'],
-}));
+app.use(
+  cors({
+    origin: ['https://localhost:5173', 'https://localhost:4173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'X-Auth-Token',
+      'Origin',
+      'Client-Security-Token',
+      'Accept',
+    ],
+  })
+)
 
 app.use(bodyParser.json())
 connectToDb()
@@ -29,7 +39,6 @@ const clientsRoutes = require('./routes/clientsRoutes')
 const appointmentsRoutes = require('./routes/appointmentsRoutes')
 const stocksRoutes = require('./routes/stocksRoutes')
 
-
 app.use('/api', authRoutes)
 app.use('/api/materials', materialsRoutes)
 app.use('/api/services', servicesRoutes)
@@ -38,10 +47,10 @@ app.use('/api/appointments', appointmentsRoutes)
 app.use('/api/stocks', stocksRoutes)
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send({ error: 'Something broke!' });
-  next();
-});
+  console.error(err.stack)
+  res.status(500).send({ error: 'Something broke!' })
+  next()
+})
 
 if (process.env.NODE_ENV === 'development') {
   const PORT = process.env.PORT || 3000
